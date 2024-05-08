95e5a44b0b714dccce6506b299356a001c9d4b2d
LaloG19 <21362134@utcancun.edu.mx>
2024-05-07 23:51:10 -0500

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { TextField, Typography, CircularProgress, Button, Box, Stack } from '@mui/material';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [displayText, setDisplayText] = useState("First React App");
+
+  const handleButtonClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      const filledBasicElement = document.getElementById("filled-basic");
+      if (filledBasicElement instanceof HTMLInputElement && filledBasicElement.value) {
+        setDisplayText(filledBasicElement.value);
+        filledBasicElement.value = '';
+      }
+    }, 500);
+  };
+
+  const handleKeyPress = (event: { key: string; }) => {
+    if (event.key === 'Enter') {
+      handleButtonClick();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        backgroundColor: '#e5e5e5',
+      }}>
+        <Stack sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '20rem',
+          backgroundColor: '#f5f5f5',
+          padding: '6rem',
+          borderRadius: '2rem',
+          boxShadow: '0.5rem 0.5rem 0.5rem #999'
+        }}>
+          <Typography variant="h1" gutterBottom sx={{ color: '#61DAFB', textDecoration: 'underline'}} >{displayText}</Typography>
+          <TextField id="filled-basic" label="Titulo" variant="filled" sx={{ marginBottom: '2rem', width: '40rem', borderRadius: '2rem', color: '#fff' }} onKeyDown={handleKeyPress}/>
+          {!loading && <Button variant="contained" onClick={handleButtonClick} sx={{ marginBottom: '2rem', backgroundColor: '#3b96af' }}> Cambiar titulo </Button>}
+          {loading && <CircularProgress color="success" sx={{ marginTop: '2rem'}} />}
+        </Stack>
+
+      </Box>
+    </>
+  )
+};
 
 export default App;
---
+++ src/notes/instructions.md
@@ -0,0 +1,12 @@
+# Comandos básicos
+- *Crear* proyecto de *React*
+> npm create-react-app first-app --template typescript 
+
+- Modifique mi componente *App*
+
+- Instalar *eslint*
+> npx eslint --init
+
+- Instalar *Material UI* y sus _dependencias_ de React
+> npm install @mui/material @emoticon/react @emotion/styled
+> npm install @mui/icons-material
